ddf909d04d1cc43a1b4892c9785e92121851cd21
popalsya <joepasqalee@gmail.com>
2021-04-07 01:49:59 +0500

    Добавьте файлы проекта.

---
+++ Lib/ScreenReader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Speech.Synthesis;
+using System.Windows;
+using System.Windows.Automation;
+
+namespace Notepad_Screen_Reader.Lib
+{
+    static class ScreenReader
+    {
+        private static IProgress<string> Progress { get; set; }
+
+        private static SpeechSynthesizer Speech { get; set; }
+
+        static ScreenReader()
+        {
+            Progress = new Progress<string>(s =>
+            {
+                MainWindow.Instange.Log.Text += (s + "\n");
+                MainWindow.Instange.Log.ScrollToEnd();
+            });
+
+            Speech = new SpeechSynthesizer();
+        }
+
+        public static void StartEventWatcher(object sender, RoutedEventArgs args)
+        {
+            Automation.AddAutomationFocusChangedEventHandler(OnFocusChanged);
+        }
+
+        private static void OnFocusChanged(object sender, AutomationFocusChangedEventArgs e)
+        {
+            var element = sender as AutomationElement;
+            if (element == null) return;
+
+            var process = Process.GetProcessById(element.Current.ProcessId);
+            if (process.ProcessName != "notepad") return;
+
+            if (element.Current.ControlType.ProgrammaticName != "ControlType.MenuItem") return;
+
+            Progress.Report(element.Current.Name);
+
+            Speech.SpeakAsyncCancelAll();
+            Speech.SpeakAsync(element.Current.Name);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Automation;
+using System.Speech.Synthesis;
+using Notepad_Screen_Reader.Lib;
+
+namespace Notepad_Screen_Reader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static MainWindow Instange { get; private set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            if (Instange == null) Instange = this;
+
+            Loaded += ScreenReader.StartEventWatcher;
+
+            Closed += (s, e) => Environment.Exit(0);
+        }
+    }
+}
